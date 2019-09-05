fea1a2ef6de7d39f7ad10d60ee79803f53628a65
Nikola Vuković <sterlu@users.noreply.github.com>
2019-09-05 21:16:14 +0200

    Add CORS to api

--- network/server.py
+++ network/server.py
@@ -23,4 +23,15 @@ def server_suggest():
     return jsonify(generate(title_prefix, 3))
 
 
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+app.after_request(add_cors_headers)
+
+
 app.run(port=5000)
