8b8fc1129d67a8b5f0825f654416ed7a242a616f
sterlu <sterlu.sd@gmail.com>
2019-09-10 14:00:05 +0200

    Footer twitter link

--- web/src/Footer/Footer.jsx
+++ web/src/Footer/Footer.jsx
@@ -4,13 +4,19 @@ import './Footer.scss';
 import logo from '../Header/logo.svg';
 
 const Footer = () => (
-  <div className="footer-wrapper container flex-container wrap">
-    <img alt="logo" src={logo} height="50" />
-    <div>
-      <Link to="/about">
-        O Projektu
-      </Link>
+  <div className="footer-wrapper container">
+    <div className="flex-container">
+      <img alt="logo" src={logo} height="50" />
+      <div>
+        <Link to="/about">O Projektu</Link>
+        <a href="https://twitter.com/FlesNjuz" className="twitter" target="_blank" />
+      </div>
     </div>
+    <p>
+      Sve prikazane naslove je generisala veštačka inteligencija. Sadržaj sajta ni u kom pogledu
+      ne prikazuju istinu niti se odnosi na stvarne događaje i ličnosti. Svaka sličnost sa stvarnim
+      aktuelnim dešavanjima je slučajna.
+    </p>
   </div>
 );
 
