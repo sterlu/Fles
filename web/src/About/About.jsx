54feecbd8bf024ec6ad89e9699b67ffe2a0a9019
sterlu <sterlu.sd@gmail.com>
2019-09-10 14:36:39 +0200

    About page

--- web/src/About/About.jsx
+++ web/src/About/About.jsx
@@ -6,18 +6,11 @@ const About = () => (
   <div className="about-wrapper">
     <h1>O projektu <b>Fleš Njuz</b></h1>
     <p>
-      Instalacija Fleš nastala je inspirisana fenomenom lažnih vesti.
-      <br />
-      <br />
-      Prvo je formirana baza sa prikupljenih 750.000 tekstova sa informativnih portala u Srbiji.
-      Zatim je formiran nov informativni portal (tzv. VI portal lažnih vesti) gde su generisani novi
-      tekstovi pomoću dubinskog učenja, a iz pomenute baze tekstova. Portal je osmišljen sa svim
-      funkcijama koje ima prosečan informativni portal, tako da posetioci mogu da ostavljaju
-      komentare, dele članke po mreži, lajkuju i sl. Radi apsolutne verodostojnosti/istinosti
-      informativnog portala, tj. njegovog stvarnog postojanja, odštampan je primerak novina sa
-      presekom vesti u jednom danu.
-      <br />
-      <br />
+      Fleš Njuz projekat je nastao za <a href="https://muzejnt.rs/sutra/" target="_blank">SUTRA
+      Festival</a> inspirisanan fenomenom lažnih vesti. Prikazane vesti generiše neuronska mreža
+      obučena uz pomoć 700.000 naslova sa domaćih informativnih portala.
+    </p>
+    <p>
       Rad je odgovor na vreme i okruženje u kom živimo, u vreme postistine, kada smo svakodnevno
       zasuti nepreglednom količinom informaciji od kojih je veliki broj lažan. Zbog zasutosti i
       opsednutošću tehnologijom većina konzumenata ne nalazi prostor za samorefleksiju niti može da
@@ -26,12 +19,10 @@ const About = () => (
       mediji.
     </p>
 
-    <a href="https://muzejnt.rs/sutra/portfolio-item/fles/">
-      https://muzejnt.rs/sutra/portfolio-item/fles/
-    </a>
-
-    <br />
-    <br />
+    <p>
+      Projekat su realizovali <a href="https://github.com/sterlu" target="_blank">Nikola
+      Vuković</a> i <a href="http://lomz.net" target="_blank">Vukašin Stančević</a>
+    </p>
 
     <Link to="/">Nazad</Link>
   </div>
