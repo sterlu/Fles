d37a86b63ca997d7900956752130a93a0cd8a7b9
sterlu <sterlu.sd@gmail.com>
2020-01-21 15:46:37 +0100

    Fix subroute deployment

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -9,5 +9,8 @@ module.exports = (config) => {
     }
   };
   config.module.rules[config.module.rules.length-1].oneOf.splice(0, 0, csvRule);
+  if (process.env.REACT_APP_GHPAGES) {
+    config.output.publicPath = ''
+  }
   return config;
 };
